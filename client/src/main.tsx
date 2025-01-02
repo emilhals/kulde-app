2db54c6b12f34fb1c57d35a81c82339c5121c406
emilhals <emilhals.i@gmail.com>
2025-01-02 15:08:27 +0100

    add tailwindcss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css'
 import App from './App.tsx'
 import Home from './pages/Home.tsx'
-
+import Simulator from './pages/Simulator.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} /> 
+      <Route path="/" element={<Home />} />
+      <Route path="/Simulator" element={<Simulator />} /> 
     </Routes>
   
   </BrowserRouter>
--- client/src/pages/Simulator.tsx
+++ client/src/pages/Simulator.tsx
@@ -1,5 +1,10 @@
 function Simulator() {
 
+  return(
+    <>
+      <h3 className="underline">Simulator</h3>
+    </>
+  )
 }
 
 export default Simulator
