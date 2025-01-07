cd724d2183ff0084c535075d31deed343461394e
emilhals <emilhals.i@gmail.com>
2025-01-07 09:45:29 +0100

    More temps and components

--- client/src/components/simulator/Compressor.tsx
+++ client/src/components/simulator/Compressor.tsx
@@ -4,7 +4,7 @@ function Compressor() {
   return(
     <>
       <Text fontSize={30} x={window.innerWidth / 3.05} y={870} text="Compressor" align="center" wrap="char" width={700}/>
-      <Rect x={window.innerWidth / 2} y={750} width={100} height={100} fill="green" />
+      <Rect x={window.innerWidth / 2} y={750} width={100} height={100} fill="black" />
     </>
   )
 }
--- client/src/pages/Simulator.tsx
+++ client/src/pages/Simulator.tsx
@@ -1,22 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Stage, Layer } from 'react-konva'
 
 import Compressor from "../components/simulator/Compressor.tsx"
+import Evaporator from "../components/simulator/Evaporator.tsx"
 
 function Simulator() {
   /* general */
-  const [refrigerant, setRefrigerant] = useState("R404A")
-  const [cooling, setCooling] = useState(false)
+  const [refrigerant, setRefrigerant] = useState<string>("R404A")
+  const [cooling, setCooling] = useState<Boolean>(false)
+
+  /* regulator */
+  const [SP, setSP] = useState<number>(4)
 
   /* temperatures */
-  const [temperature, setTemperature] = useState(24)
-  const [SP, setSP] = useState(4)
+  const [temperature, setTemperature] = useState<number>(24)
+  const [evaporator, setEvaporator] = useState<number>(24)
+  const [condensor, setCondensor] = useState<number>(24)
+  const [SH, setSH] = useState<number>()
 
-  /* pressures */ 
-  const [LP, setLP] = useState()
-  const [HP, setHP] = useState()
-  
-  const getPressures = (temperature) => {
+  /* pressures */
+  const [LP, setLP] = useState<number>()
+  const [HP, setHP] = useState<number>()
+  const [LP_temp, setLP_temp] = useState<number>(0)
+
+  const getPressures = (temperature: number) => {
     fetch(`http://localhost:8000/simulator/${refrigerant}/${temperature}/get-pressures`).then(res => res.json()).then(data => {
       setLP(data.LP / 1e5)
       setHP(data.HP / 1e5)
@@ -24,35 +31,50 @@ function Simulator() {
   }
 
   useEffect(() => {
-    let interval;
+    let interval: ReturnType<typeof setTimeout>;
     if (cooling && temperature > SP) {
       interval = setInterval(() => {
-        setTemperature(temperature => temperature - 1);
-        getPressures(temperature);
+        setTemperature(temperature => temperature - 1)
+        getPressures(temperature)
+
+        if (evaporator > -4) {
+          setEvaporator(evaporator => evaporator - 2)
+        }
+
+        if (condensor < 35) {
+          setCondensor(condensor => condensor + 2)
+        }
+
+        setSH(evaporator - LP_temp)
+
       }, 2000);
     }
 
     return () => clearInterval(interval);
   }, [temperature, cooling])
 
-  return(
+  return (
     <div className="container mx-auto px-4">
       <h3 className="font-bold text-4xl fixed top-5"> Simulator.</h3>
-      <div>
-      <h3 className="font-bold text-xl">Information</h3>
-      <p>Room Temperature: {temperature}</p>
-      <p>Set Point: {SP}</p>
-      <p>Refrigerant: {refrigerant}</p>
-      <p>LP: {LP}</p>
-      <p>HP: {HP}</p>
- 
-      <button onClick={() => setCooling(!cooling)}>{cooling ? 'Stop' : 'Start'}</button>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-            <Compressor />
-        </Layer>
-      </Stage>    
+      <div className="fixed top-5 right-5">
+        <h3 className="font-bold text-xl">Information</h3>
+        <p>Room Temperature: {temperature}</p>
+        <p>Set Point: {SP}</p>
+        <p>Refrigerant: {refrigerant}</p>
+        <p>LP: {LP?.toFixed(2)} bar</p>
+        <p>HP: {HP?.toFixed(2)} bar</p>
+        <p>Evaporator: {evaporator} C</p>
+        <p>Condensor: {condensor} C</p>
+        <p>Superheat: {SH}</p>
       </div>
+
+      <button className="z-1" onClick={() => setCooling(!cooling)}>{cooling ? 'Stop' : 'Start'}</button>
+      <Stage width={window.innerWidth - 50} height={1000}>
+        <Layer>
+          <Evaporator />
+          <Compressor />
+        </Layer>
+      </Stage>
     </div>
   )
 }
