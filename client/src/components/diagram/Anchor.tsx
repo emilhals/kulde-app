0b624667a9735bbcce8197b5945c78c1dd241517
emilhals <emilhals.i@gmail.com>
2025-03-16 11:15:08 +0100

    added hover animation and types

--- client/src/components/diagram/Anchor.tsx
+++ client/src/components/diagram/Anchor.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import Konva from 'konva'
 import { Circle } from 'react-konva'
 
@@ -9,8 +9,35 @@ const dragBounds = (ref: React.RefObject<Konva.Circle>) => {
   return { x: 0, y: 0 }
 }
 
-export const Anchor = ({ x, y, id, onDragMove, onDragStart, onDragEnd, handleClick }) => {
+type PropsType = {
+  x: number
+  y: number
+  id: string
+  hovered: string
+  onDragStart: (e: Konva.KonvaEventObject<DragEvent>, id: string | number) => void
+  onDragMove: (e: Konva.KonvaEventObject<DragEvent>, id: string | number) => void
+  onDragEnd: (e: Konva.KonvaEventObject<DragEvent>, id: string | number) => void
+}
+
+export const Anchor = ({ x, y, id, hovered, onDragMove, onDragStart, onDragEnd }: PropsType) => {
   const anchor = useRef<Konva.Circle>(null)
+  const hover = useRef<Konva.Circle>(null)
+
+  useEffect(() => {
+    if (hovered === id) {
+      hover.current?.to({
+        radius: 9,
+        duration: 0.2,
+        easing: Konva.Easings.EaseIn
+      })
+    } else {
+      hover.current?.to({
+        radius: 5,
+        duration: 0.2,
+        easing: Konva.Easings.EaseOut
+      })
+    }
+  }, [hovered])
 
   return (
     <>
@@ -19,16 +46,28 @@ export const Anchor = ({ x, y, id, onDragMove, onDragStart, onDragEnd, handleCli
         x={x}
         y={y}
         radius={5}
-        fill='black'
+        fill='#E83F6F'
+        opacity={0.5}
+        visible={hovered === id}
+        ref={hover}
+      />
+
+      <Circle
+        id={id}
+        x={x}
+        y={y}
+        radius={5}
+        fill='#E83F6F'
         draggable
-        onClick={handleClick}
         onDragStart={(e) => onDragStart(e, id)}
         onDragMove={(e) => onDragMove(e, id)}
         onDragEnd={(e) => onDragEnd(e, id)}
         dragBoundFunc={() => dragBounds(anchor)}
         perfectDrawEnabled={false}
+        name='anchor'
         ref={anchor}
       />
+
     </>
   )
 }
