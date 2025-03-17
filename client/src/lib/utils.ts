7c12128cc8bd022f2e42bdbbe66d356b444f42bb
emilhals <emilhals.i@gmail.com>
2025-03-17 23:55:38 +0100

    created getConnectionPoints helper

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,19 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-import { ItemType, PointType } from '@/common/types'
+import { ItemType, PointType, ConnectionType } from '@/common/types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
-
-
 /*
- * For the connecting related components
+ * For the connection related components
  */
-export const createConnectionPoints = (source: PointType, anchor: string, destination: PointType) => {
+export const createConnectionPoints = (destination: PointType, sourceAnchor: string, destinationAnchor: string) => {
   let OFFSET = 200
 
   let nullPoint: PointType = {
@@ -26,22 +23,89 @@ export const createConnectionPoints = (source: PointType, anchor: string, destin
     y: 0
   }
 
-  if ((destination.x - nullPoint.x) < OFFSET) {
-    midPoint.x = destination.x
-    midPoint.y = destination.y
-  } else {
-    midPoint.x = destination.x
-    midPoint.y = 0
-  }
+  if (destinationAnchor) {
+    if (sourceAnchor.x() !== destinationAnchor.x()) {
+      if (sourceAnchor.id() === 'right' && destinationAnchor.id() === 'left') {
+        return [destination.x / 2, 0, destination.x / 2, destination.y, destination.x, destination.y]
+      }
+      if (sourceAnchor.id() === 'right' && destinationAnchor.id() === 'bottom') {
+        return [destination.x, 0, destination.x, destination.y]
+      }
+      if (sourceAnchor.id() === 'right' && destinationAnchor.id() === 'right') {
+        return [destination.x + 200, 0, destination.x + 200, destination.y, destination.x, destination.y]
+      }
+      if (sourceAnchor.id() === 'right' && destinationAnchor.id() === 'top') {
+        return [destination.x / 2, 0, destination.x / 2, destination.y - 100, destination.x, destination.y - 100, destination.x, destination.y]
+      }
 
-  return [nullPoint.x, nullPoint.y, midPoint.x, midPoint.y, destination.x, destination.y]
+      /*
+      if (sourceAnchor.id() === 'top' && destinationAnchor.id() === 'left') {
+        return [destination.x / 2, 0, destination.x / 2, destination.y, destination.x, destination.y]
+      }*/
+      if (sourceAnchor.id() === 'top' && destinationAnchor.id() === 'bottom') {
+        return [0, destination.y - 100, destination.x, destination.y + 100, destination.x, destination.y + 100, destination.x, destination.y]
+      }
+      if (sourceAnchor.id() === 'top' && destinationAnchor.id() === 'right') {
+        return [0, destination.y, destination.x, destination.y, destination.x, destination.y]
+      }
+      if (sourceAnchor.id() === 'top' && destinationAnchor.id() === 'top') {
+        return [0, destination.y - 100, destination.x, destination.y - 100, destination.x, destination.y - 100, destination.x, destination.y]
+      }
+
+    }
+  } else {
+    if ((destination.x - nullPoint.x) > OFFSET) {
+      return [nullPoint.x, nullPoint.y, destination.x / 2, 0, destination.x / 2, destination.y, destination.x, destination.y]
+    }
+    return [nullPoint.x, nullPoint.y, midPoint.x, midPoint.y, destination.x, destination.y]
+  }
 }
 
-export const hasIntersection = (position: PointType, item: ItemType) => {
+export const getConnectionPoints = (from: ItemType, to: ItemType, connection: ConnectionType) => {
+  return [from.x, from.y, connection.points.at(2), from.y, to.x, to.y]
+}
+
+
+export const getOffset = (placement: string, item: ItemType) => {
+  switch (placement) {
+    case 'top':
+      return {
+        x: item.width / 2,
+        y: 0
+      }
+
+    case 'bottom':
+      return {
+        x: item.width / 2,
+        y: item.height,
+      }
+
+    case 'right':
+      return {
+        x: item.width,
+        y: item.height / 2,
+      }
+
+    case 'left':
+      return {
+        x: 0,
+        y: item.height / 2,
+      }
+    default:
+      return {
+        x: 0,
+        y: 0
+      }
+  }
+}
+
+export const hasIntersection = (position: PointType, item) => {
+  let OFFSET = 50
+
   return !(
-    item.x > position.x ||
-    item.x + item.width < position.x ||
-    item.y > position.y ||
-    item.y + item.height < position.y
+    item.x() - OFFSET > position.x ||
+    item.x() + (item.width() + OFFSET) < position.x ||
+    item.y() - OFFSET > position.y ||
+    item.y() + (item.height() + OFFSET) < position.y
   )
 }
