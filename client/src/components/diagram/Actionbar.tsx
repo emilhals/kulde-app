6d4244b64d031816801f85e36700ba7eeede74d0
emilhals <emilhals.i@gmail.com>
2025-03-17 22:12:50 +0100

    fixed hydration issue

--- client/src/components/diagram/Actionbar.tsx
+++ client/src/components/diagram/Actionbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext } from 'react'
 
 import {
   Command,
@@ -31,31 +31,29 @@ import { cn } from "@/lib/utils"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-
-
 import { Check, ChevronsUpDown, Spline, SquareDashedMousePointer, Type, Plus, Download, Bold, Italic, Underline } from 'lucide-react'
-import { ACTIONS } from '@/common/constants'
-import { ItemType, TextType } from '@/common/types'
-import { ActionContext } from '@/common/Providers'
 
-import { proxy } from 'valtio'
+import { ActionContext } from '@/common/Providers'
+import { ACTIONS } from '@/common/constants'
 
 import { useAddToStore } from '@/hooks/useAddToStore'
+import { ItemType, TextType } from '@/common/types'
+import { X509Certificate } from 'crypto'
 
 const COMPONENTS = [
   {
     value: "compressor",
     label: "Compressor",
     img: "compressor.png",
-    width: 124,
-    height: 124,
+    width: 96,
+    height: 96,
   },
   {
     value: "condensator",
     label: "Condensator",
-    img: "condensor.png",
-    width: 124,
-    height: 124,
+    img: "condensator.png",
+    width: 96,
+    height: 96,
   },
   {
     value: "evaporator",
@@ -73,15 +71,14 @@ const COMPONENTS = [
   },
 ]
 
-/* placeholders for text creation */
+
 let text: TextType = {
   id: '', /* set in useAddToStore */
   text: '',
   size: 16,
-  color: 'black',
   x: 0,
   y: 0,
-  parentId: '',
+  standalone: true,
   attributes: {
     bold: false,
     italic: false,
@@ -89,8 +86,7 @@ let text: TextType = {
   }
 }
 
-/* placeholder for item creation */
-let item = proxy<ItemType>({
+let item: ItemType = {
   id: '', /* set in useAddToStore */
   img: '',
   textXOffset: 0,
@@ -101,21 +97,11 @@ let item = proxy<ItemType>({
   y: 64,
   text: text,
   type: ''
-})
-
-
-/*
- *
- * TODO: fiks typescript greiene
- *
- * */
-
+}
 
 export const Actionbar = () => {
   const [open, setOpen] = useState(false)
   const [value, setValue] = useState("")
-
-  const [itemLabel, setItemLabel] = useState('')
 
   const [textPlaceholder, setTextPlaceholder] = useState('')
   const [textAttributes, setTextAttributes] = useState({ isBold: false, isItalic: false, isUnderlined: false })
@@ -123,28 +109,48 @@ export const Actionbar = () => {
   const [component, setComponent] = useState('')
 
   const actionContext = useContext(ActionContext)
-  /* from navigation menu. used for item creation */
-  item.text = text
-  item.text.text = itemLabel
-  item.type = component.type
-  item.width = component.width
-  item.height = component.height
-  item.img = component.img
 
-  text.bold = textAttributes.isBold
-  text.italic = textAttributes.isItalic
-  text.underline = textAttributes.isUnderlined
-  text.text = textPlaceholder
-  text.x = 50
-  text.y = 50
-  text.independent = true
-  text.text = textPlaceholder
+  const handleCreate = (data: ItemType | TextType) => {
+
+    text.attributes.bold = textAttributes.isBold
+    text.attributes.italic = textAttributes.isItalic
+    text.attributes.underline = textAttributes.isUnderlined
+    text.text = textPlaceholder
+    text.x = 50
+    text.y = 50
+
+    if ('img' in data) {
+      console.log('ya')
+
+      item.type = component.type
+      item.width = component.width
+      item.height = component.height
+      item.img = component.img
+      item.text.standalone = false
+      item.text = text
+
+
+      useAddToStore(data)
+    } else if ('standalone' in data) {
+      text.standalone = true
+
+      useAddToStore(data)
+    }
+
+    setTextPlaceholder('')
+
+    console.log(data)
+  }
+
+  console.log(actionContext?.action)
+
 
   return (
     <div className="flex justify-center items-center gap-4 px-3 w-fit mx-auto border shadow-lg rounded-lg">
       <TooltipProvider>
+
         <Tooltip>
-          <TooltipTrigger>
+          <TooltipTrigger asChild>
             <button onClick={() => { actionContext?.updateAction(ACTIONS.SELECT) }} className={actionContext?.action === 'SELECT' ? "bg-violet-300 p-1 rounded" : "p-1 hover:bg-violet-100 rounded"}>
               <SquareDashedMousePointer className="size-5"></SquareDashedMousePointer>
             </button>
@@ -153,20 +159,24 @@ export const Actionbar = () => {
             <p>Select</p>
           </TooltipContent>
         </Tooltip>
+
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem className={actionContext?.action === ACTIONS.ADD ? "bg-white p-1 rounded" : "p-1 hover:bg-white rounded"}>
-              <NavigationMenuTrigger className={actionContext?.action === ACTIONS.ADD ? "bg-white p-1 rounded" : "p-1 hover:bg-white rounded"} onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}>
-                <Tooltip>
-                  <TooltipTrigger onClick={() => { actionContext?.updateAction(ACTIONS.ADD) }} className={actionContext?.action === ACTIONS.ADD ? "bg-violet-300 p-1 rounded" : "p-1 hover:bg-violet-100 rounded"}>
-                    <Plus className="justify-center size-5"></Plus>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Add item</p>
-                  </TooltipContent>
-                </Tooltip>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent onPointerEnter={(e) => e.preventDefault()} onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}>
+            <NavigationMenuItem>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <NavigationMenuTrigger onClick={() => { actionContext?.updateAction(ACTIONS.ADD) }} className={actionContext?.action === 'ADD' ? 'bg-white p-1 rounded' : 'p-1 hover:bg-violet-100 rounded'} onPointerMove={(e) => { e.preventDefault() }} onPointerLeave={(e) => { e.preventDefault() }}>
+                    <span>
+                      <Plus className={actionContext?.action === 'ADD' ? 'size-5 text-rose-300' : ' size-5'}></Plus>
+                    </span>
+                  </NavigationMenuTrigger>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Add item</p>
+                </TooltipContent>
+              </Tooltip>
+
+              <NavigationMenuContent>
                 <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3">
                     <NavigationMenuLink asChild>
@@ -178,7 +188,7 @@ export const Actionbar = () => {
                       </a>
                     </NavigationMenuLink>
                   </li>
-                  <Input type="text" id="item-label" value={itemLabel} onChange={e => setItemLabel((e.target.value))} placeholder="Label" />
+                  <Input type="text" id="item-label" value={textPlaceholder} onChange={e => setTextPlaceholder((e.target.value))} placeholder="Label" />
                   <Popover open={open} onOpenChange={setOpen}>
                     <PopoverTrigger asChild>
                       <Button
@@ -223,7 +233,7 @@ export const Actionbar = () => {
                       </Command>
                     </PopoverContent>
                   </Popover>
-                  <Button onClick={() => { useAddToStore(item) }} variant="outline">
+                  <Button onClick={() => { handleCreate(item) }} variant="outline">
                     Add component
                   </Button>
                 </ul>
@@ -234,17 +244,17 @@ export const Actionbar = () => {
 
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem className={actionContext?.action === ACTIONS.WRITE ? "bg-white p-1 rounded" : "p-1 hover:bg-white rounded"}>
-              <NavigationMenuTrigger className={actionContext?.action === ACTIONS.WRITE ? "bg-white p-1 rounded" : "p-1 hover:bg-white rounded"} onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}>
-                <Tooltip>
-                  <TooltipTrigger onClick={() => { actionContext?.updateAction(ACTIONS.WRITE) }} className={actionContext?.action === ACTIONS.WRITE ? "bg-violet-300 p-1 rounded" : "p-1 hover:bg-violet-100 rounded"}>
+            <NavigationMenuItem>
+              <Tooltip>
+                <TooltipTrigger asChild onClick={() => { actionContext?.updateAction(ACTIONS.WRITE) }} className={actionContext?.action === ACTIONS.WRITE ? "bg-violet-300 p-1 rounded" : "p-1 hover:bg-violet-100 rounded"}>
+                  <NavigationMenuTrigger onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}>
                     <Type className="justify-center size-5"></Type>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Add text</p>
-                  </TooltipContent>
-                </Tooltip>
-              </NavigationMenuTrigger>
+                  </NavigationMenuTrigger>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Add text</p>
+                </TooltipContent>
+              </Tooltip>
               <NavigationMenuContent onPointerEnter={(e) => e.preventDefault()} onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}>
                 <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3">
@@ -269,7 +279,7 @@ export const Actionbar = () => {
                       <Underline className="h-4 w-4" />
                     </ToggleGroupItem>
                   </ToggleGroup>
-                  <Button onClick={() => { useAddToStore(text) }} variant="outline">
+                  <Button onClick={() => { handleCreate(text) }} variant="outline">
                     Add text
                   </Button>
                 </ul>
@@ -280,7 +290,7 @@ export const Actionbar = () => {
 
 
         <Tooltip>
-          <TooltipTrigger>
+          <TooltipTrigger asChild>
             <button onClick={() => actionContext.updateAction(ACTIONS.CONNECTOR)} className={actionContext.action === ACTIONS.CONNECTOR ? "bg-violet-300 p-1 rounded" : "p-1 hover:bg-violet-100 rounded"} >
               <Spline className="size-5"></Spline>
             </button>
@@ -290,7 +300,7 @@ export const Actionbar = () => {
           </TooltipContent>
         </Tooltip>
         <Tooltip>
-          <TooltipTrigger>
+          <TooltipTrigger asChild>
             <button onClick={() => { console.log("download") }} className="p-1 rounded hover:bg-violet-100" >
               <Download className="size-5"></Download>
             </button>
@@ -299,7 +309,8 @@ export const Actionbar = () => {
             <p>Download</p>
           </TooltipContent>
         </Tooltip>
+
       </TooltipProvider>
-    </div>
+    </div >
   )
 }
