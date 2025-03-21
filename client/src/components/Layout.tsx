c67a1fc05a5caefc899472714a3dc8d0eeb1d3b2
emilhals <emilhals.i@gmail.com>
2025-03-22 00:30:36 +0100

    fixed export mistake

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 import Navigation from "./Navigation"
 
-export const Layout = () => {
+const Layout = () => {
   return (
     <div className='flex flex-col h-screen overflow-hidden'>
       <div className='grow-0'>
@@ -14,3 +14,5 @@ export const Layout = () => {
     </div>
   )
 }
+
+export default Layout
