6c192c8222416743c8937b3df98ed8f2e81d58b2
emilhals <emilhals.i@gmail.com>
2025-03-24 19:41:28 +0100

    now also delete lines

--- client/src/hooks/useDeleteFromStore.ts
+++ client/src/hooks/useDeleteFromStore.ts
@@ -2,6 +2,10 @@ import { store } from '@/store'
 
 export const useDeleteFromStore = (id: string) => {
   const index = store.items.findIndex((item) => item.id === id)
+  const connection = store.connections.findIndex((conn) => conn.from.id === id || conn.to.id === id)
+
+  if (connection >= 0)
+    store.connections.splice(connection, 10)
 
   if (index >= 0)
     store.items.splice(index, 1)
