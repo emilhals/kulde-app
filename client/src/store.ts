69914d15d0546c6713305d4deee06551daac02a7
emilhals <emilhals.i@gmail.com>
2025-03-24 09:34:29 +0100

    added selected to store to track the selected item

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,9 +1,10 @@
-import { proxy } from "valtio"
+import { proxy } from 'valtio'
 
-import { ItemType, ConnectionType, TextType } from "./common/types"
+import { ItemType, ConnectionType, TextType } from '@/common/types'
 
-export const store = proxy<{ items: ItemType[], connections: ConnectionType[], texts: TextType[] }>({
+export const store = proxy<{ items: ItemType[], connections: ConnectionType[], texts: TextType[], selected: string }>({
   items: [],
   connections: [],
   texts: [],
+  selected: ''
 })
