52c675e07ce1b135778001af680215c3eb043510
emilhals <emilhals.i@gmail.com>
2025-04-07 12:41:35 +0200

    fix: use id from proxy instead of index as key

--- client/src/pages/Diagram.tsx
+++ client/src/pages/Diagram.tsx
@@ -1,154 +1,111 @@
-import { Stage, Layer, Transformer, Line } from 'react-konva'
+import { useEffect, useRef, useState } from 'react'
+
+import { Stage, Layer } from 'react-konva'
+import { Html } from 'react-konva-utils'
 import Konva from 'konva'
-import { useEffect, useRef, useState, useContext } from 'react'
 
 import { useSnapshot } from 'valtio'
 import { store } from '@/store'
 
-import { ACTIONS } from '@/common/constants'
 
 import { Text } from '@/components/diagram/Text'
 import { Item } from '@/components/diagram/Item'
 import { Connector } from '@/components/diagram/Connector'
-import { Actionbar } from '@/components/diagram/Actionbar'
+import { Line } from '@/components/diagram/Line'
+import { Create } from '@/components/diagram/Create'
 
-import { ActionContext } from '@/common/Providers'
 import { useCustomFont } from '@/hooks/useCustomFont'
 
 import { getConnectionPoints } from '@/lib/utils'
+import { KonvaEventObject } from 'konva/lib/Node'
+import { Toolbar } from '@/components/diagram/Toolbar'
 
 const DiagramPage = () => {
   const stageRef = useRef<Konva.Stage>(null)
   const containerRef = useRef<HTMLDivElement>(null)
   const itemLayer = useRef<Konva.Layer>(null)
-  const transformerRef = useRef<Konva.Transformer>(null)
   const connectorRef = useRef<Konva.Line>(null)
 
   /* loading the custom font */
   const [,] = useCustomFont('Open Sans')
 
-  const [size, setSize] = useState({ width: window.innerWidth, height: 900 })
+  const [stage, setStage] = useState({ width: 0, height: 0, scale: 1 })
 
   const snap = useSnapshot(store)
-  const actionContext = useContext(ActionContext)
 
   /* set stage size and ensure responsiveness  */
   useEffect(() => {
-    if (!containerRef.current) return
-
-    let scale = containerRef.current.offsetWidth / size.width
-    const checkSize = () => {
-      let stageWidth = size.width * scale
-      let stageHeight = size.height * scale
-
-      setSize({
-        width: stageWidth,
-        height: stageHeight
+    const resize = () => {
+      if (!containerRef.current) return
+      const { clientWidth, clientHeight } = containerRef.current
+      setStage({
+        scale: 1,
+        width: clientWidth,
+        height: clientHeight
       })
     }
 
-    window.addEventListener('resize', checkSize)
-    return () => window.removeEventListener('resize', checkSize)
+    resize()
+    window.addEventListener('resize', resize)
+    return () => window.removeEventListener('resize', resize)
   }, [])
 
-  let stageWidth = size.width % 2 !== 0 ? size.width - 1 : size.width
-  let stageHeight = size.height % 2 !== 0 ? size.height - 1 : size.height
+  const handleClick = (e: KonvaEventObject<Konva.KonvaPointerEvent>) => {
+    if (!e.target.id())
+      store.selected = null
+  }
+
+  const handleMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.button === 1) {
+      console.log('hei')
+      e.preventDefault()
+    }
+  }
+
+
 
   return (
-    <div ref={containerRef} className="flex flex-col dark:bg-dark-bg" onContextMenu={(e) => { e.preventDefault() }}>
-      <div className="grow">
-        <Actionbar />
-        <Stage
-          className="w-full border border-black dark:border-gray-700 bg-[radial-gradient(#D9D9D9_1px,transparent_1px)] dark:bg-[radial-gradient(#2a2a2a_1px,transparent_1px)] bg-[length:16px_16px]"
-          ref={stageRef}
-          width={stageWidth} height={stageHeight}
-          onContextMenu={(e) => { e.evt.preventDefault() }}
-          context
-        >
-          <Layer ref={itemLayer}>
-            {snap.items
-              .map((item, index) => {
-                return (
-                  <Item key={index} item={item} />
-                )
-              }
-              )}
+    <div ref={containerRef} className='h-full' onAuxClick={handleMove} onContextMenu={(e) => { e.preventDefault() }}>
+      <Create />
+      <Toolbar stage={stageRef} />
+      <Stage
+        className="w-full z-10 bg-[radial-gradient(#D9D9D9_1px,transparent_1px)] dark:bg-[radial-gradient(#2a2a2a_1px,transparent_1px)] bg-[length:16px_16px]"
+        ref={stageRef}
+        width={stage.width}
+        height={stage.height}
+        scaleX={stage.scale}
+        scaleY={stage.scale}
+        onContextMenu={(e) => { e.evt.preventDefault() }}
+        onClick={handleClick}
+      >
 
-            {snap.connections
-              .map((connection, index) => {
-                const points = getConnectionPoints(connection.from, connection.to, connection)
-                return (
-                  <Line
-                    key={index}
-                    x={connection.from.x + connection.offsets.from.x}
-                    y={connection.from.y + connection.offsets.from.y}
-                    ref={connectorRef}
-                    points={points}
-                    stroke="black"
-                    strokeWidth={2}
-                  />
-                )
-              })}
+        <Layer ref={itemLayer}>
+          {snap.items
+            .map((item) => {
+              return (
+                <Item key={item.id} item={item} />
+              )
+            }
+            )}
 
-            {snap.texts
-              .map((text, index) => {
-                return (
-                  <Text key={index} parent={text} standalone={true} />
-                )
-              })}
+          {snap.connections
+            .map((connection) => {
+              return (
+                <Line key={connection.id} connection={connection} />
+              )
+            })}
 
-            <Transformer ref={transformerRef} />
-          </Layer>
+          {snap.texts
+            .map((text) => {
+              return (
+                <Text key={text.id} parent={text} standalone={true} />
+              )
+            })}
 
-          {actionContext?.action === ACTIONS.CONNECTOR && (
-            <Connector stageRef={stageRef} layerRef={itemLayer} />
-          )}
+        </Layer>
 
-        </Stage>
-      </div>
-
-      <div className="h-32 grow-0 bg-gray-50 dark:bg-dark-bg">
-        <h3>Components:</h3>
-        <div className="flex-row">
-          <div className="basis-1/4">
-            {snap.items
-              .map((item, index) => {
-                return (
-                  <ul key={index}>
-                    <li>{item.id} - {item.text.text}</li>
-                    <li>x: {item.x} | y: {item.y}</li>
-                  </ul>
-                )
-              }
-              )}
-          </div>
-
-          <div className="basis-1/4">
-            {snap.texts
-              .map((text, index) => {
-                return (
-                  <ul key={index}>
-                    <li>{text.id} - {text.text}</li>
-                    <li>x: {text.x} | y: {text.y}</li>
-                  </ul>
-                )
-              }
-              )}
-          </div>
-
-          <div className="basis-1/4">
-            {snap.connections
-              .map((connection, index) => {
-                return (
-                  <ul key={index}>
-                    <li>{connection.id}</li>
-                  </ul>
-                )
-              }
-              )}
-          </div>
-        </div>
-      </div>
+        <Connector stageRef={stageRef} selected={snap.selected} />
+      </Stage>
     </div>
   )
 }
