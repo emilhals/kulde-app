a0eef5becbe1a2a1eb2dcb459d305ecce44b3a08
emilhals <emilhals.i@gmail.com>
2025-09-12 13:15:56 +0200

    fix: remove x and y positioning on Rect

--- client/src/features/diagram-drawer/canvas/symbols/SightGlass.tsx
+++ client/src/features/diagram-drawer/canvas/symbols/SightGlass.tsx
@@ -4,22 +4,19 @@ import { ItemType } from '@/features/diagram-drawer/types'
 const SightGlass = ({ item }: { item: ItemType }) => {
   if (!item) return
 
-  const centerX = item.x + item.width / 2
-  const centerY = item.y + item.height / 2
-
   return (
-    <Group>
+    <Group id={item.id}>
       <Rect
-        x={item.x}
-        y={item.y}
+        id={item.id}
         width={item.width}
         height={item.height}
         stroke="black"
         strokeWidth={2}
       />
       <Ring
-        x={centerX}
-        y={centerY}
+        id={item.id}
+        x={item.width / 2}
+        y={item.height / 2}
         innerRadius={5}
         outerRadius={8}
         strokeWidth={2}
