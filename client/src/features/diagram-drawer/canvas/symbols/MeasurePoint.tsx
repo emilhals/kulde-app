5b5f579b3709fc6fd41c234c6a0888fed3daaa5a
emilhals <emilhals.i@gmail.com>
2025-09-14 09:37:10 +0200

    feat: add MeasurePoint

---
+++ client/src/features/diagram-drawer/canvas/symbols/MeasurePoint.tsx
@@ -0,0 +1,44 @@
+import { Circle, Group, Text } from 'react-konva'
+import { ItemType } from '@/features/diagram-drawer/types'
+
+const MeasurePoint = ({ item }: { item: ItemType }) => {
+  const centerX = item.width / 2
+  const centerY = item.height / 2
+
+  const radius = Math.max(item.height, item.width) / 2
+
+  const needle = item.component.search('measurepoint_t')
+  let label = ''
+
+  if (needle === -1) {
+    label = 'P'
+  } else {
+    label = 'T'
+  }
+
+  return (
+    <Group id={item.id}>
+      <Text
+        id={item.id}
+        text={label}
+        fontSize={16}
+        fontFamily={'Inter'}
+        x={centerX}
+        y={centerY}
+        align="center"
+        offsetX={5}
+        offsetY={7}
+      />
+      <Circle
+        id={item.id}
+        x={centerX}
+        y={centerY}
+        radius={radius}
+        stroke="black"
+        strokeWidth={2}
+      />
+    </Group>
+  )
+}
+
+export default MeasurePoint
