289bdc1b5b6c1f31d5948ee1b53d7631ea0f0ebf
emilhals <emilhals.i@gmail.com>
2025-09-15 20:24:52 +0200

    feat: add useConnectionPreview

---
+++ client/src/features/diagram-drawer/hooks/useConnectionPreview.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import { Line } from 'react-konva'
+
+import { Shape } from 'konva/lib/Shape'
+import { Node, NodeConfig } from 'konva/lib/Node'
+
+import { PointType } from '@/features/diagram-drawer/types'
+import { createConnectionPoints } from '@/features/diagram-drawer/utils/createConnectionPoints'
+
+export const useConnectionPreview = () => {
+  const [connectionPreview, setConnectionPreview] = useState<React.ReactNode>()
+
+  const setInitialPreview = (position: PointType) => {
+    setConnectionPreview(
+      <Line
+        x={position.x}
+        y={position.y}
+        points={[position.x, position.y, position.x, position.y]}
+        strokeWidth={4}
+        stroke="#2d9cdb"
+        lineJoin="round"
+        lineCap="round"
+        perfectDrawEnabled={false}
+        shadowBlur={1}
+        shadowColor="#ffffff"
+        shadowOpacity={0.5}
+        listening={false}
+      />,
+    )
+  }
+
+  const updatePreview = (
+    position: PointType,
+    mousePosition: PointType,
+    draggedFromAnchor: Node<NodeConfig>,
+    hoveredAnchor: Shape,
+  ) => {
+    setConnectionPreview(
+      <Line
+        x={position.x}
+        y={position.y}
+        points={[0, 0].concat(
+          createConnectionPoints(
+            mousePosition,
+            draggedFromAnchor,
+            hoveredAnchor,
+          ),
+        )}
+        strokeWidth={3}
+        stroke="#2d9cdb"
+        lineJoin="round"
+        lineCap="round"
+        listening={false}
+        perfectDrawEnabled={false}
+        shadowBlur={10}
+        shadowOffsetY={1}
+        shadowOffsetX={1}
+        shadowColor="#42AEEC"
+        shadowOpacity={0.5}
+      />,
+    )
+  }
+
+  const clearPreview = () => setConnectionPreview(null)
+
+  return {
+    connectionPreview,
+    setInitialPreview,
+    updatePreview,
+    clearPreview,
+  }
+}
