c2e4f774a69c0854dd5e1c81ffce3af1bedc5250
emilhals <emilhals.i@gmail.com>
2025-09-20 19:31:58 +0200

    feat: add removeFromStore and addToStore

--- client/src/features/diagram-drawer/store.ts
+++ client/src/features/diagram-drawer/store.ts
@@ -4,7 +4,12 @@ import {
   ItemType,
   ConnectionType,
   TextType,
+  ItemPreview,
+  TextPreview,
+  ConnectionPreview,
 } from '@/features/diagram-drawer/types'
+
+import { v4 as uuidv4 } from 'uuid'
 
 const initialState = {
   items: [] as ItemType[],
@@ -12,9 +17,46 @@ const initialState = {
   texts: [] as TextType[],
   selected: null as ItemType | null,
   dragged: '',
+  action: '',
 }
 
 export const store = proxy(deepClone(initialState))
+
+export const addToStore = (
+  property: ItemPreview | TextPreview | ConnectionPreview,
+) => {
+  if (!property || !property.type) return null
+
+  const id = uuidv4()
+
+  if (property.type === 'items') {
+    const newItem = { ...property, id: id }
+    store.items.push(newItem)
+    return newItem
+  }
+
+  if (property.type === 'texts') {
+    const newText = store.texts.push({ ...property, id: id })
+    return newText
+  }
+
+  if (property.type === 'connections') {
+    const newConnection = store.connections.push({ ...property, id: id })
+    return newConnection
+  }
+}
+
+export const removeFromStore = (
+  property: ItemType | TextType | ConnectionType,
+) => {
+  const index = store[property.type].findIndex(
+    (object) => object.id === property.id,
+  )
+
+  if (index >= 0) {
+    store[property.type].splice(index, 1)
+  }
+}
 
 export const clearStore = () => {
   Object.assign(store, deepClone(initialState))
