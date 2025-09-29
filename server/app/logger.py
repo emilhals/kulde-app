b654b73c5e1e15db36e126fcedf2405a30691f07
emilhals <emilhals.i@gmail.com>
2025-09-29 17:18:52 +0200

    feat: add Logger with config

---
+++ server/app/logger.py
@@ -0,0 +1,15 @@
+import logging.config
+
+import pathlib
+import json
+
+import logging
+
+def load_config() -> None:
+    config_file = pathlib.Path('app/logging_config.json')
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
