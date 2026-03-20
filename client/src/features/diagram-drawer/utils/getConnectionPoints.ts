2a6762355a98810aa02377b0f14be8348f06a545
emilhals <emilhals.i@gmail.com>
2026-03-20 02:51:28 +0100

    feat(diagram-drawer): add getConnectionPoints

--- client/src/features/diagram-drawer/utils/getConnectionPoints.ts
+++ client/src/features/diagram-drawer/utils/getConnectionPoints.ts
@@ -2,56 +2,76 @@ import { ItemType, ConnectionType } from '@/features/diagram-drawer/types'
 
 import { getAnchorOffset } from '@/features/diagram-drawer/utils/helpers'
 
+export const interpolateMidPoint = (start: number, end: number) => {
+    return start + (end - start) / 2
+}
+
 export const getConnectionPoints = (
-  from: ItemType,
-  to: ItemType,
-  connection: ConnectionType,
+    from: ItemType,
+    to: ItemType,
+    connection: ConnectionType,
 ) => {
-  const { offsetX: startAnchorOffsetX, offsetY: startAnchorOffsetY } =
-    getAnchorOffset(from)
-  const { offsetX: endAnchorOffsetX, offsetY: endAnchorOffsetY } =
-    getAnchorOffset(to)
+    const { offsetX: startAnchorOffsetX, offsetY: startAnchorOffsetY } =
+        getAnchorOffset(from)
+    const { offsetX: endAnchorOffsetX, offsetY: endAnchorOffsetY } =
+        getAnchorOffset(to)
 
-  const dx = Math.abs(from.x + startAnchorOffsetX - to.x + endAnchorOffsetX)
-  const dy = Math.abs(from.y + startAnchorOffsetY - to.y + endAnchorOffsetY)
+    const startPlacement = connection.offsets.from.placement
+    const endPlacement = connection.offsets.to.placement
 
-  const y = from.y + startAnchorOffsetY - to.y + endAnchorOffsetY
-  const x = from.x + startAnchorOffsetX - to.x + endAnchorOffsetX
+    const startX =
+        from.x + connection.offsets.from.position.x + startAnchorOffsetX
+    const startY =
+        from.y + connection.offsets.from.position.y + startAnchorOffsetY
 
-  const startX =
-    from.x + connection.offsets.from.position.x + startAnchorOffsetX
-  const startY =
-    from.y + connection.offsets.from.position.y + startAnchorOffsetY
+    const endX = to.x + connection.offsets.to.position.x + endAnchorOffsetX
+    const endY = to.y + connection.offsets.to.position.y + endAnchorOffsetY
 
-  const endX = to.x + connection.offsets.to.position.x + endAnchorOffsetX
-  const endY = to.y + connection.offsets.to.position.y + endAnchorOffsetY
-  /*
-  console.log('y: ', y)
-  console.log('dx: ', dx)
-  console.log('dy: ', dy)
-  console.log('x: ', x)
-*/
-  /* if the shapes are on about the same Y axis */
-  if (dy <= 50) {
-    console.log('same line')
+    const dy = Math.abs(startY - endY)
+    const dx = Math.abs(startX - endX)
+
+    if (dy < 5 || dx < 5) {
+        return [startX, startY, endX, endY]
+    }
+
+    const midX = interpolateMidPoint(startX, endX)
+    const midY = interpolateMidPoint(startY, endY)
+
+    if (
+        (startPlacement === 'Left' || startPlacement === 'Right') &&
+        (endPlacement === 'Left' || endPlacement === 'Right')
+    ) {
+        return [startX, startY, midX, startY, midX, endY, endX, endY]
+    }
+    if (
+        (startPlacement === 'Top' || startPlacement === 'Bottom') &&
+        (endPlacement === 'Top' || endPlacement === 'Bottom')
+    ) {
+        return [startX, startY, startX, midY, endX, midY, endX, endY]
+    }
+
+    if (
+        (startPlacement === 'Right' && endPlacement === 'Left') ||
+        (startPlacement === 'Left' && endPlacement === 'Right')
+    ) {
+        return [startX, startY, midX, startY, midX, endY, endX, endY]
+    }
+
+    if (
+        (startPlacement === 'Left' || startPlacement === 'Right') &&
+        (endPlacement === 'Top' || endPlacement === 'Bottom')
+    ) {
+        return [startX, startY, endX, startY, endX, endY]
+    }
+
+    if (
+        (startPlacement === 'Top' && endPlacement === 'Bottom') ||
+        (startPlacement === 'Bottom' && endPlacement === 'Top')
+    ) {
+        const midY = interpolateMidPoint(startY, endY)
+
+        return [startX, startY, startX, midY, endX, midY, endX, endY]
+    }
+
     return [startX, startY, endX, endY]
-  }
-
-  if (x > 0) {
-    return [startX, startY, startX / 2, startY, startX / 2, endY, endX, endY]
-  } else {
-    return [startX, startY, endX / 2, startY, endX / 2, endY, endX, endY]
-  }
-
-  /*
-      return [
-        to.x - from.width,
-        to.y,
-        to.x / 2,
-        to.y,
-        from.x / 2,
-        from.y,
-        from.x - from.width,
-        from.y,
-      ]*/
 }
