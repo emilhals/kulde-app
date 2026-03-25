3dd2f1cf55ca0e5b85a0387ce56976894cd3da58
emilhals <emilhals.i@gmail.com>
2026-03-25 12:12:44 +0100

    refactor(diagram-drawer): handle dragging with global state and pointers instead of Konvas drag system

--- client/src/features/diagram-drawer/canvas/Item.tsx
+++ client/src/features/diagram-drawer/canvas/Item.tsx
@@ -1,128 +1,82 @@
-import { useRef, useState } from 'react'
-import { Group, Rect } from 'react-konva'
+import { useRef } from 'react'
+import { Group } from 'react-konva'
 import Konva from 'konva'
 
 import { SYMBOL_MAP } from '@/features/diagram-drawer/canvas/symbols/SymbolMap'
 import { uiState, diagramHistory } from '@/features/diagram-drawer/store'
-import { ItemType, PointType } from '@/features/diagram-drawer/types'
+import { ItemType } from '@/features/diagram-drawer/types'
+import { useSnapshot } from 'valtio'
 
 export const Item = ({ item }: { item: ItemType }) => {
     const groupRef = useRef<Konva.Group>(null)
-    const shadowRef = useRef<Konva.Rect>(null)
 
-    const [shadowPosition, setShadowPosition] = useState<PointType>({
-        x: -10000,
-        y: -10000,
-    })
+    const uiSnap = useSnapshot(uiState)
+    const isDragging = uiSnap.dragging && uiSnap.selected?.id === item.id
 
     const Symbol = SYMBOL_MAP[item.component]
 
     const proxyItem = diagramHistory.value.items.find((i) => i.id === item.id)
     if (!proxyItem) return null
 
-    const handleOnPointerDown = (e: Konva.KonvaEventObject<PointerEvent>) => {
+    const handlePointerDown = (e: Konva.KonvaEventObject<PointerEvent>) => {
+        const stage = e.target.getStage()
+        const pointer = stage?.getPointerPosition()
+        if (!pointer) return
+
         uiState.selected = proxyItem
 
-        shadowRef.current?.hide()
-        setShadowPosition({
-            x: Math.round(e.target.getAbsolutePosition().x / 16) * 16,
-            y: Math.round(e.target.getAbsolutePosition().y / 16) * 16,
-        })
-    }
+        uiState.pointerDown = true
+        uiState.pointerStart = pointer
 
-    const handleDragMove = (e: Konva.KonvaEventObject<DragEvent>) => {
-        shadowRef.current?.show()
+        uiState.dragOffset = {
+            x: pointer.x - item.x,
+            y: pointer.y - item.y,
+        }
 
-        setShadowPosition({
-            x: Math.round(e.target.getAbsolutePosition().x / 16) * 16,
-            y: Math.round(e.target.getAbsolutePosition().y / 16) * 16,
-        })
-
-        proxyItem.x = e.target.getAbsolutePosition().x
-        proxyItem.y = e.target.getAbsolutePosition().y
-
-        diagramHistory.value.connections.forEach((conn) => {
-            if (conn.from && conn.from.id === item.id) {
-                conn.from = proxyItem
-            }
-            if (conn.to && conn.to.id === item.id) {
-                conn.to = proxyItem
-            }
-        })
-    }
-
-    const handleDragEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
-        shadowRef.current?.hide()
-
-        /* snap to dot-grid */
-        proxyItem.x = Math.round(e.target.getAbsolutePosition().x / 16) * 16
-        proxyItem.y = Math.round(e.target.getAbsolutePosition().y / 16) * 16
-
-        setShadowPosition({
-            x: Math.round(e.target.getAbsolutePosition().x / 16) * 16,
-            y: Math.round(e.target.getAbsolutePosition().y / 16) * 16,
-        })
-
-        diagramHistory.value.connections.forEach((conn) => {
-            if (conn.from && conn.from.id === proxyItem.id) {
-                conn.from = proxyItem
-            }
-            if (conn.to && conn?.to.id === proxyItem.id) {
-                conn.to = proxyItem
-
-                if (!conn.from) return null
-                if (
-                    conn.from.y - conn.to.y < 20 &&
-                    conn.from.y - conn.to.y > -21
-                ) {
-                    conn.to.y = conn.from.y
-                }
-            }
-        })
+        uiState.shadowPosition = {
+            x: item.x,
+            y: item.y,
+        }
     }
 
     return (
-        <Group
-            ref={groupRef}
-            id={item.id}
-            draggable={!item.locked}
-            x={item.x}
-            y={item.y}
-            onPointerDown={handleOnPointerDown}
-            onDragMove={handleDragMove}
-            onDragEnd={handleDragEnd}
-            onContextMenu={(e) => {
-                e.evt.preventDefault()
-            }}
-            onMouseEnter={(e) => {
-                const container = e.target.getStage()?.container()
-                if (!container) return
+        <>
+            {!isDragging && (
+                <Group
+                    ref={groupRef}
+                    id={item.id}
+                    x={item.x}
+                    y={item.y}
+                    onPointerDown={handlePointerDown}
+                    onContextMenu={(e) => {
+                        e.evt.preventDefault()
+                    }}
+                    onMouseEnter={(e) => {
+                        const container = e.target.getStage()?.container()
+                        if (!container) return
 
-                container.style.cursor = 'grab'
-            }}
-            onMouseLeave={(e) => {
-                const container = e.target.getStage()?.container()
-                if (!container) return
+                        container.style.cursor = 'grab'
+                    }}
+                    onMouseLeave={(e) => {
+                        const container = e.target.getStage()?.container()
+                        if (!container) return
 
-                container.style.cursor = 'default'
-            }}
-        >
-            <Rect
-                ref={shadowRef}
-                name="shadow"
-                x={shadowPosition.x - item.x}
-                y={shadowPosition.y - item.y}
-                height={item.height}
-                width={item.width}
-                strokeWidth={2}
-                stroke="black"
-                opacity={0.2}
-                onPointerDown={handleOnPointerDown}
-                onDragMove={handleDragMove}
-                onDragEnd={handleDragEnd}
-            />
+                        container.style.cursor = 'default'
+                    }}
+                >
+                    <Symbol item={item} />
+                </Group>
+            )}
 
-            <Symbol item={item} />
-        </Group>
+            {isDragging && (
+                <Group
+                    x={uiSnap.shadowPosition.x}
+                    y={uiSnap.shadowPosition.y}
+                    opacity={0.6}
+                >
+                    <Symbol item={item} />
+                </Group>
+            )}
+        </>
     )
 }
