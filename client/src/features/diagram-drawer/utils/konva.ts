b186584a473206b72bf91a75c518da90ae784272
emilhals <emilhals.i@gmail.com>
2026-04-01 05:49:08 +0200

    feat(diagram-drawer): add function for finding intersection

--- client/src/features/diagram-drawer/utils/konva.ts
+++ client/src/features/diagram-drawer/utils/konva.ts
@@ -1,3 +1,4 @@
+import { Rect } from '@/features/diagram-drawer/types'
 import Konva from 'konva'
 
 export const dragBounds = (ref: React.RefObject<Konva.Circle>) => {
@@ -5,4 +6,13 @@ export const dragBounds = (ref: React.RefObject<Konva.Circle>) => {
         return ref.current.getAbsolutePosition()
     }
     return { x: 0, y: 0 }
+}
+
+export const intersected = (selection: Rect, item: Rect): boolean => {
+    return (
+        selection.left < item.right &&
+        selection.right > item.left &&
+        selection.top < item.bottom &&
+        selection.bottom > item.top
+    )
 }
