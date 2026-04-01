f1c3616f19087960ce6988b3cbbc0cb6069cd558
emilhals <emilhals.i@gmail.com>
2026-04-01 05:37:44 +0200

    refactor(diagram-drawer): move pointer events to page

--- client/src/features/diagram-drawer/canvas/Item.tsx
+++ client/src/features/diagram-drawer/canvas/Item.tsx
@@ -1,9 +1,9 @@
-import { useRef } from 'react'
-import { Group } from 'react-konva'
 import Konva from 'konva'
+import { useRef } from 'react'
+import { Group, Line } from 'react-konva'
 
 import { SYMBOL_MAP } from '@/features/diagram-drawer/canvas/symbols/SymbolMap'
-import { uiState, diagramHistory } from '@/features/diagram-drawer/store'
+import { uiState } from '@/features/diagram-drawer/store/models'
 import { ItemType } from '@/features/diagram-drawer/types'
 import { useSnapshot } from 'valtio'
 
@@ -11,72 +11,65 @@ export const Item = ({ item }: { item: ItemType }) => {
     const groupRef = useRef<Konva.Group>(null)
 
     const uiSnap = useSnapshot(uiState)
-    const isDragging = uiSnap.dragging && uiSnap.selected?.id === item.id
+    const isDragging =
+        uiSnap.interaction === 'dragging-item' && uiSnap.activeId === item.id
 
     const Symbol = SYMBOL_MAP[item.component]
 
-    const proxyItem = diagramHistory.value.items.find((i) => i.id === item.id)
-    if (!proxyItem) return null
-
-    const handlePointerDown = (e: Konva.KonvaEventObject<PointerEvent>) => {
-        const stage = e.target.getStage()
-        const pointer = stage?.getPointerPosition()
-        if (!pointer) return
-
-        uiState.selected = proxyItem
-
-        uiState.pointerDown = true
-        uiState.pointerStart = pointer
-
-        uiState.dragOffset = {
-            x: pointer.x - item.x,
-            y: pointer.y - item.y,
-        }
-
-        uiState.shadowPosition = {
-            x: item.x,
-            y: item.y,
-        }
-    }
+    const borderPoints = [
+        0,
+        0,
+        item.width,
+        0,
+        item.width,
+        item.height,
+        0,
+        item.height,
+        0,
+        0,
+    ]
 
     return (
         <>
-            {!isDragging && (
-                <Group
-                    ref={groupRef}
-                    id={item.id}
-                    x={item.x}
-                    y={item.y}
-                    onPointerDown={handlePointerDown}
-                    onContextMenu={(e) => {
-                        e.evt.preventDefault()
-                    }}
-                    onMouseEnter={(e) => {
-                        const container = e.target.getStage()?.container()
-                        if (!container) return
+            <Group
+                ref={groupRef}
+                name="item"
+                data-type="item"
+                id={item.id}
+                x={item.x}
+                y={item.y}
+                opacity={isDragging ? 0.75 : 1}
+                shadowBlur={isDragging ? 10 : 0}
+                shadowOpacity={0.2}
+                onContextMenu={(e) => {
+                    e.evt.preventDefault()
+                }}
+                onMouseEnter={(e) => {
+                    const container = e.target.getStage()?.container()
+                    if (!container) return
 
-                        container.style.cursor = 'grab'
-                    }}
-                    onMouseLeave={(e) => {
-                        const container = e.target.getStage()?.container()
-                        if (!container) return
+                    container.style.cursor = 'grab'
+                }}
+                onMouseLeave={(e) => {
+                    const container = e.target.getStage()?.container()
+                    if (!container) return
 
-                        container.style.cursor = 'default'
-                    }}
-                >
-                    <Symbol item={item} />
-                </Group>
-            )}
+                    container.style.cursor = 'default'
+                }}
+            >
+                {isDragging && (
+                    <Line
+                        points={borderPoints}
+                        stroke="#7C3AED"
+                        dash={[6, 4]}
+                        strokeWidth={2}
+                        listening={false}
+                        perfectDrawEnabled={false}
+                    />
+                )}
 
-            {isDragging && (
-                <Group
-                    x={uiSnap.shadowPosition.x}
-                    y={uiSnap.shadowPosition.y}
-                    opacity={0.6}
-                >
-                    <Symbol item={item} />
-                </Group>
-            )}
+                <Symbol item={item} />
+            </Group>
         </>
     )
 }
