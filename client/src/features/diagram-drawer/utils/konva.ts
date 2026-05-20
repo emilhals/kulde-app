ca2f51868ba8dfea4e0679334ea0de1bc256e734
emilhals <113390306+emilhals@users.noreply.github.com>
2026-05-20 21:26:48 +0200

    feat(diagram-drawer): add util function for downloading canvas

--- client/src/features/diagram-drawer/utils/konva.ts
+++ client/src/features/diagram-drawer/utils/konva.ts
@@ -2,17 +2,27 @@ import { Rect } from '@/features/diagram-drawer/types'
 import Konva from 'konva'
 
 export const dragBounds = (ref: React.RefObject<Konva.Circle>) => {
-    if (ref.current !== null) {
-        return ref.current.getAbsolutePosition()
-    }
-    return { x: 0, y: 0 }
+	if (ref.current !== null) {
+		return ref.current.getAbsolutePosition()
+	}
+	return { x: 0, y: 0 }
 }
 
 export const intersected = (selection: Rect, item: Rect): boolean => {
-    return (
-        selection.left < item.right &&
-        selection.right > item.left &&
-        selection.top < item.bottom &&
-        selection.bottom > item.top
-    )
+	return (
+		selection.left < item.right &&
+		selection.right > item.left &&
+		selection.top < item.bottom &&
+		selection.bottom > item.top
+	)
+}
+
+// function from https://stackoverflow.com/a/15832662/512042
+export const downloadURI = (uri: string, name: string) => {
+	var link = document.createElement('a')
+	link.download = name
+	link.href = uri
+	document.body.appendChild(link)
+	link.click()
+	document.body.removeChild(link)
 }
