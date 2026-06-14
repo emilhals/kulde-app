afba503e02beab34c5508be0a0eb066afebd37b7
emilhals <113390306+emilhals@users.noreply.github.com>
2026-06-14 15:31:31 +0200

    feat(simulator): add types for SimulationStatus and extend component types

--- client/src/features/simulator/types.ts
+++ client/src/features/simulator/types.ts
@@ -1,13 +1,25 @@
+import Konva from 'konva'
+
+export type Point = { x: number; y: number }
+
+export type SimulationStatus = 'RUNNING' | 'STOPPING' | 'RESTARTING' | 'IDLE'
+
+export type Particle = Konva.Circle & { velocity: Point; direction: number }
+
 export type Compressor = {
   power_state: string
   run_state: string
   discharge_pressure: number
   discharge_temp: number
+  suction_pressure: number
+  suction_temperature: number
 }
 
 export type Evaporator = {
   suction_pressure: number
   suction_temperature: number
+  overheat: number
+  fan_speed: number
 }
 
 export type Condensator = {
@@ -15,4 +27,11 @@ export type Condensator = {
   condensing_temperature: number
   liquid_temp: number
   subcooling: number
+  fan_speed: number
+}
+
+export type SystemState = {
+  isCooling: boolean
+  isDefrosting: boolean
+  runningFans: boolean
 }
