b0e970b68f373c9fe41fd71c8a98da5218a02f5b
emilhals <113390306+emilhals@users.noreply.github.com>
2026-07-30 18:48:03 +0200

    refactor(simulator): move logic into their own components or hooks

--- client/src/features/simulator/page.tsx
+++ client/src/features/simulator/page.tsx
@@ -1,9 +1,12 @@
+import { useCustomFont } from '@/features/shared/hooks/useCustomFont'
+import { useResponsiveStage } from '@/features/shared/hooks/useResponsiveStage'
 import { Compressor } from '@/features/simulator/canvas/Compressor'
 import { HeatExchanger } from '@/features/simulator/canvas/HeatExchanger'
 import { PipeNetwork } from '@/features/simulator/canvas/PipeNetwork'
 import { PressureGauge } from '@/features/simulator/canvas/PressureGauge'
 import { TEV } from '@/features/simulator/canvas/TEV'
-import { PowerState, RunState } from '@/features/simulator/constants/enums'
+import { motion } from 'motion/react'
+
 import {
   compressorPosition,
   condenserPosition,
@@ -11,278 +14,174 @@ import {
   pipes,
   tevPosition,
 } from '@/features/simulator/physics/positions'
-import {
-  flattenParams,
-  resetControllerState,
-} from '@/features/simulator/store/actions'
-import {
-  controllerState,
-  initialControllerState,
-} from '@/features/simulator/store/models'
-import type {
-  Compressor as CompressorType,
-  Condensator,
-  Evaporator,
-  SimulationSpeed,
-  SystemState,
-} from '@/features/simulator/types'
+import type { Point } from '@/features/simulator/types'
 import { Controller } from '@/features/simulator/ui/Controller'
-import { SimulationControls } from '@/features/simulator/ui/SimulationControls'
-import {
-  DEFAULT_COMPRESSOR,
-  DEFAULT_CONDENSATOR,
-  DEFAULT_EVAPORATOR,
-} from '@/features/simulator/utils/default.components'
-import { parseSimulationData } from '@/features/simulator/utils/parseSimulationData'
-import { useCustomFont } from '@/shared/hooks/useCustomFont'
-import { useResponsiveStage } from '@/shared/hooks/useResponsiveStage'
+import { useClickAway } from '@uidotdev/usehooks'
 import Konva from 'konva'
 import { useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Layer, Stage, Text } from 'react-konva'
-import useWebSocket, { ReadyState } from 'react-use-websocket-lite'
-import { subscribe, useSnapshot } from 'valtio'
+import { Group, Layer, Stage } from 'react-konva'
+import { useSnapshot } from 'valtio'
+import { useSimulationSocket } from './hooks/useSimulationSocket'
+import { simulationState } from './stores/simulation'
+import { uiState } from './stores/ui'
+import { InformationPanel } from './ui/InformationPanel'
+import { InstructionsPanel } from './ui/InstructionsPanel'
+import { ParametersPanel } from './ui/ParametersPanel'
+import { PlaybackButton } from './ui/PlaybackButton'
+import { SimulationMenu } from './ui/SimulationMenu'
+import { setActivePanel } from './stores/ui.actions'
 
 export const SimulatorPage = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation('simulator')
 
   const [,] = useCustomFont('Inter')
-  const url = import.meta.env.VITE_WS_URL
+  const {
+    connectionStatus,
+    startSimulation,
+    restartSimulation,
+    stopSimulation,
+  } = useSimulationSocket()
 
-  const controllerSnap = useSnapshot(controllerState)
+  const uiSnap = useSnapshot(uiState)
+  const simulationSnap = useSnapshot(simulationState)
+  const systemSnap = useSnapshot(simulationState.system)
 
-  const restartingRef = useRef<boolean>(false)
+  const controls = simulationSnap.controls
+  const components = simulationSnap.components
 
-  const stageRef = useRef<Konva.Stage>(null)
   const containerRef = useRef<HTMLDivElement>(null)
-
   const stage = useResponsiveStage(containerRef)
 
-  const [simulationSpeed, setSimulationSpeed] =
-    useState<SimulationSpeed>('normal')
-  const [systemState, setSystemState] = useState<SystemState>({
-    isCooling: false,
-    isDefrosting: false,
-    runningFans: false,
-  })
-  const [roomTemp, setRoomTemp] = useState<string>('24.2')
-
-  const [compressor, setCompressor] =
-    useState<CompressorType>(DEFAULT_COMPRESSOR)
-  const [evaporator, setEvaporator] = useState<Evaporator>(DEFAULT_EVAPORATOR)
-  const [condensator, setCondensator] =
-    useState<Condensator>(DEFAULT_CONDENSATOR)
-
-  const { sendMessage, readyState } = useWebSocket({
-    url: url,
-    onClose() {
-      setSimulationStatus('idle')
-    },
-    onMessage(event) {
-      if (restartingRef.current) return
-
-      const data = JSON.parse(event.data)
-
-      if (data.status === 'STOPPED') {
-        setEvaporator({ ...evaporator, fan_speed: 0 })
-        setCondensator({ ...condensator, fan_speed: 0 })
-        restartingRef.current = false
-        setSimulationStatus('idle')
-        setSystemState({
-          isCooling: false,
-          isDefrosting: false,
-          runningFans: false,
-        })
-
-        return
-      }
-
-      if (data.status === 'running') {
-        setSimulationStatus('running')
-      }
-
-      const parsed = parseSimulationData(data)
-      if (parsed === null || parsed === undefined) return
-
-      setRoomTemp(parsed.roomTemp)
-      setEvaporator(parsed.evaporator)
-      setCondensator(parsed.condensator)
-      setCompressor(parsed.compressor)
-
-      console.log(parsed.compressor)
-
-      setSystemState((prev) => ({
-        ...prev,
-        isCooling: parsed.compressor.run_state === RunState.RUNNING,
-        runningFans: parsed.compressor.power_state === PowerState.ON,
-      }))
-    },
+  const parametersPanelRef = useClickAway<HTMLDivElement>(() => {
+    if (
+      uiState.activePanel === 'parameters' &&
+      !uiState.panels.parameters.isPinned
+    ) {
+      setActivePanel(null)
+    }
   })
 
-  const [simulationStatus, setSimulationStatus] = useState<
-    'running' | 'stopping' | 'restarting' | 'idle'
-  >('idle')
+  const instructionsPanelRef = useClickAway<HTMLDivElement>(() => {
+    if (
+      uiState.activePanel === 'instructions' &&
+      !uiState.panels.instructions.isPinned
+    ) {
+      setActivePanel(null)
+    }
+  })
+
+  const groupRef = useRef<Konva.Group>(null)
+  const [groupPosition, setGroupPosition] = useState<Point>({ x: 0, y: 0 })
 
   useEffect(() => {
-    const unsubscribe = subscribe(controllerState, () => {
-      if (readyState !== ReadyState.OPEN) return
-      sendMessage(
-        JSON.stringify({
-          command: 'UPDATE_PARAMS',
-          controllerParams: flattenParams(controllerState),
-        }),
-      )
-    })
+    if (!groupRef.current || !stage) return
+    const group = groupRef.current
 
-    return () => unsubscribe()
-  }, [readyState, sendMessage])
+    const { width, height } = group.getClientRect()
+    const x = stage.width / 2 - width / 2
+    const y = stage.height / 2 - height / 2
 
-  const handleSpeedChange = (speed: SimulationSpeed) => {
-    let speedNum = 10
-
-    switch (speed) {
-      case 'slow':
-        speedNum = 5
-        break
-      case 'normal':
-        speedNum = 10
-        break
-      case 'fast':
-        speedNum = 30
-        break
-      default:
-        speedNum = 10
-    }
-    setSimulationSpeed(speed)
-
-    sendMessage(JSON.stringify({ command: 'UPDATE_SPEED', speed: speedNum }))
-  }
-
-  const handleStart = () => {
-    if (readyState !== ReadyState.OPEN) return
-
-    sendMessage(
-      JSON.stringify({
-        command: 'START',
-        controllerParams: flattenParams(controllerSnap),
-      }),
-    )
-
-    setSimulationStatus('running')
-  }
-
-  const handleStop = () => {
-    if (readyState !== ReadyState.OPEN) return
-
-    sendMessage(JSON.stringify({ command: 'STOP' }))
-
-    setSimulationStatus('stopping')
-  }
-
-  const handleRestart = () => {
-    if (readyState !== ReadyState.OPEN) return
-
-    resetControllerState()
-
-    setRoomTemp('24.0')
-    sendMessage(
-      JSON.stringify({
-        command: 'RESTART',
-        controllerParams: initialControllerState,
-      }),
-    )
-
-    setSimulationStatus('restarting')
-  }
+    setGroupPosition({ x: x - 50, y: y })
+  }, [groupRef, stage])
 
   return (
-    <div className="flex flex-col gap-2 w-full h-full min-h-0">
-      <div className="flex justify-between">
-        <div className="flex px-8">
-          <SimulationControls
-            simulationStatus={simulationStatus}
-            speed={simulationSpeed}
-            onSpeedChange={handleSpeedChange}
-            onStart={handleStart}
-            onRestart={handleRestart}
-            onStop={handleStop}
+    <motion.div layout className="flex h-full min-h-0 w-full gap-2 px-2 py-2">
+      <motion.main
+        layout
+        id="container"
+        ref={containerRef}
+        tabIndex={0}
+        className={
+          'relative flex-1 overflow-hidden rounded-lg border border-gray-300 bg-zinc-100 bg-[radial-gradient(#D9D9D9_1px,transparent_1px)] bg-[length:16px_16px] focus:outline-none dark:border-slate-600 dark:bg-slate-800 dark:bg-[radial-gradient(#3a3a3a_1px,transparent_1px)]'
+        }
+      >
+        <div className="absolute left-6 top-6 z-30 flex h-full max-w-96 flex-col gap-4 transition-opacity ease-in-out">
+          <Controller roomTemp={systemSnap.roomTemp} />
+        </div>
+
+        <div ref={parametersPanelRef}>
+          {uiSnap.activePanel === 'parameters' && <ParametersPanel />}
+        </div>
+
+        <div ref={instructionsPanelRef}>
+          {uiSnap.activePanel === 'instructions' && <InstructionsPanel />}
+        </div>
+
+        <div className="absolute right-6 top-6 z-30 flex items-center gap-3">
+          <PlaybackButton onStart={startSimulation} onStop={stopSimulation} />
+          <SimulationMenu
+            onStart={startSimulation}
+            onRestart={restartSimulation}
+            onStop={stopSimulation}
           />
         </div>
 
-        <div className="flex justify-end h-32">
-          <Controller roomTemp={roomTemp} systemState={systemState} />
-        </div>
-      </div>
-
-      <div className="overflow-hidden relative flex-1 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none">
-        <div ref={containerRef} className="h-full bg-slate-100">
+        <div className="absolute flex-1">
           {stage && (
-            <Stage width={stage.width} height={stage.height} ref={stageRef}>
+            <Stage width={stage.width} height={stage.height}>
               <Layer>
-                <HeatExchanger
-                  label="Condensor"
-                  data={condensator}
-                  position={condenserPosition}
-                />
+                <Group ref={groupRef} x={groupPosition.x} y={groupPosition.y}>
+                  <Compressor
+                    position={compressorPosition}
+                    label="Compressor"
+                  />
+                  <HeatExchanger
+                    label="condensor"
+                    data={components.condensor}
+                    position={condenserPosition}
+                  />
 
-                <HeatExchanger
-                  label="Evaporator"
-                  data={condensator}
-                  position={evaporatorPosition}
-                />
-                <TEV position={tevPosition} />
+                  <TEV position={tevPosition} scale={0.75} />
+                  <HeatExchanger
+                    label="evaporator"
+                    data={components.condensor}
+                    position={evaporatorPosition}
+                  />
 
-                <PressureGauge
-                  type="HP"
-                  inlet={{ x: 400, y: 400 }}
-                  pressure={compressor.discharge_pressure}
-                />
-                <PressureGauge
-                  type="LP"
-                  inlet={{ x: 900, y: 420 }}
-                  pressure={compressor.suction_pressure}
-                />
+                  <PressureGauge
+                    type="HP"
+                    inlet={{ x: 100, y: 400 }}
+                    pressure={
+                      simulationSnap.components.compressor.discharge_pressure
+                        .value
+                    }
+                  />
+                  <PressureGauge
+                    type="LP"
+                    inlet={{ x: 440, y: 420 }}
+                    pressure={
+                      simulationSnap.components.compressor.suction_pressure
+                        .value
+                    }
+                  />
+                </Group>
               </Layer>
 
               <Layer>
-                <PipeNetwork
-                  animate={simulationStatus === 'running'}
-                  pressure={{ LP: 3, HP: 8 }}
-                  pipes={pipes}
-                />
-              </Layer>
-              <Layer>
-                <Text
-                  text={compressor.run_state}
-                  y={500}
-                  x={550}
-                  fontStyle="bold"
-                  align="center"
-                />
-
-                <Text
-                  text={compressor.power_state}
-                  y={520}
-                  x={550}
-                  fontStyle="bold"
-                  align="center"
-                />
-                <Compressor position={compressorPosition} />
+                <Group x={groupPosition.x} y={groupPosition.y}>
+                  <PipeNetwork
+                    animate={controls.status === 'running'}
+                    pressure={{ LP: 3, HP: 8 }}
+                    pipes={pipes}
+                  />
+                </Group>
               </Layer>
             </Stage>
           )}
         </div>
 
-        {readyState !== ReadyState.OPEN && (
-          <div className="flex absolute inset-0 z-40 justify-center items-center bg-white/70">
-            <div className="p-5 text-sm text-red-800 bg-red-50 rounded-md shadow">
-              {readyState === ReadyState.CONNECTING
-                ? 'Connecting to simulation server...'
-                : t('simulator.no-connection')}
+        {connectionStatus !== 'active' && (
+          <div className="absolute inset-0 z-40 flex items-center justify-center">
+            <div className="rounded-md p-5 text-sm text-red-800 shadow">
+              {connectionStatus === 'connecting'
+                ? t('connecting') + '...'
+                : t('no-connection')}
             </div>
           </div>
         )}
-      </div>
-    </div>
+      </motion.main>
+      <InformationPanel show={true} />
+    </motion.div>
   )
 }
