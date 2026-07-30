5de8eac4fc0c62f42538a3fb4b286f067e798680
emilhals <113390306+emilhals@users.noreply.github.com>
2026-07-30 18:05:36 +0200

    chore: add react-rnd package for moving and resizing panels

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
-import path from 'path'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  define: { 'process.env.DRAGGABLE_DEBUG': 'false' },
 })
